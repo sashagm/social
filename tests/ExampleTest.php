a2d2d02ec1f7eaed86441065bb09917ce6eda95a
Sashagm <example@example.com>
2023-06-07 21:56:45 +0500

    Add phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Sashagm\Social\Tests;
+
+use Sashagm\Social\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test  */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sashagm\Social\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    //
+}
+
+
+
+
+
+
