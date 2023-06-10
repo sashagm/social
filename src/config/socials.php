c7c8e0bd92d264ecdc9ff5112fa7b94886c61232
Sashagm <example@example.com>
2023-06-11 03:43:52 +0500

    update fix

--- src/config/socials.php
+++ src/config/socials.php
@@ -5,7 +5,7 @@ return [
     'admin_prefix'              => '', 
     'user_table'                => 'users',
     'user_avatar'               => "img",   
-    'socials.user_table_after'  => "password",
+    'user_table_after'  => "password",
     'isActive'                  => true,
 
 
