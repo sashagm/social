4b771e62aa1a9ab9d1208e1235300edeadf429ee
sashagm <74922473+sashagm@users.noreply.github.com>
2024-09-11 23:58:07 +0500

    fix route list

--- src/routes/social.php
+++ src/routes/social.php
@@ -8,21 +8,31 @@ $routes = config('socials.routes', []);
 
 Route::group(['middleware' => ['web', 'guest'], 'prefix' => config('socials.admin_prefix')], function () use ($routes) {
 
+    if (isset($routes['auth_login'])) {
         Route::get($routes['auth_login'][0], [LoginController::class, 'redirectToProvider'])
-                ->name($routes['auth_login'][1]);
+            ->name($routes['auth_login'][1]);
+    }
 
+    if (isset($routes['auth_login_callback'])) {
         Route::get($routes['auth_login_callback'][0], [LoginController::class, 'handleProviderCallback'])
-                ->name($routes['auth_login_callback'][1]);
+            ->name($routes['auth_login_callback'][1]);
+    }
 
+    if (isset($routes['auth_login_form'])) {
         Route::get($routes['auth_login_form'][0], [AuthController::class, 'showLoginForm'])
-                ->name($routes['auth_login_form'][1]);
+            ->name($routes['auth_login_form'][1]);
+    }
 
+    if (isset($routes['auth_login_form_callback'])) {
         Route::post($routes['auth_login_form_callback'][0], [AuthController::class, 'login'])
-                ->name($routes['auth_login_form_callback'][1]);
+            ->name($routes['auth_login_form_callback'][1]);
+    }
 });
 
 Route::group(['middleware' => ['web', 'auth']], function () use ($routes) {
 
+    if (isset($routes['social_logout'])) {
         Route::post($routes['social_logout'][0], [LoginController::class, 'logout'])
-                ->name($routes['social_logout'][1]);
+            ->name($routes['social_logout'][1]);
+    }
 });
